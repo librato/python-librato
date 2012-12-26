27d6da686c6d6d8761cf360972a4935f90b1c1af
David Rio Deiros <driodeiros@gmail.com>
2012-12-26 16:34:59 -0600

    Using Requests instead of httplib in _exec. Mocking requests. Basic testing for requesting metrics.

--- librato/connection.py
+++ librato/connection.py
@@ -1,11 +1,68 @@
+import requests
+import json
+
 # Defaults
-HOSTNAME  = "metrics-api.librato.com"
+HOSTNAME  = "https://metrics-api.librato.com"
 BASE_PATH = "/v1/"
 
+class Metric():
+  def __init__(self, connection, name, attributes=None, period=None, description=None):
+    self.connection = connection
+    self.name = name
+    #self.attributes=attributes or {}
+    #self.period = period
+    self.description = description
+
+  @classmethod
+  def from_dict(cls, connection, data):
+    """Returns a metric object from a dictionary item,
+       which is usually from librato's API"""
+    if data.get('type') == "gauge":
+      cls = Gauge
+    elif data.get('type') == "counter":
+      cls = Counter
+
+    obj             = cls(connection, data['name'])
+    obj.description = data['description']
+    #obj.period      = data['period']
+    #obj.attributes  = data['attributes']
+    obj.description = data['description']
+
+    return obj
+
+class Gauge(Metric):
+  """Librato Gauge metric"""
+
+  def add(self, value, source=None, **params):
+    """Add a new measurement to this gauge"""
+    return self.connection.send_gauge_value(self.name, value, source, **params)
+
+class Counter(Metric):
+  """Librato Counter metric"""
+
+  def add(self, value, source=None, **params):
+    return self.connection.send_counter_value(self.name, value, source, **params)
+
 class Connection(object):
+  HOSTNAME  = "https://metrics-api.librato.com"
+  BASE_PATH = "/v1/"
+
   def __init__(self, username, api_key, hostname=HOSTNAME, base_path=BASE_PATH):
-    self.username  = username
-    self.api_key   = api_key
     self.hostname  = hostname
     self.base_path = base_path
+    self.auth      = (username, api_key)
 
+  def _exe(self, end_path, query_params):
+    url      = self.hostname + self.base_path + end_path
+    response = requests.get(url, auth=self.auth)
+    return json.loads(response.text)
+
+  def _parse(self, resp, name, cls):
+    if resp.has_key(name):
+      return [cls.from_dict(self, m) for m in resp[name]]
+    else:
+      return resp
+
+  def list_metrics(self, **query_params):
+    r = self._exe(end_path="metrics", query_params=query_params)
+    return self._parse(resp=r, name="metrics", cls=Metric)
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,13 +1,42 @@
+import sys, json, unittest, re
+from urlparse import urlparse
 import librato
 
-class TestConnection:
+class MockResponse(object):
+  def __init__(self):
+    self.text = ""
+
+class MockRequest(object):
+  def __init__(self):
+    pass
+
+  def get(self, url, auth):
+    f_name    = urlparse(url).path.replace('/', '_')
+    fd        = open('tests/fixtures/%s.json' % f_name)
+    content   = fd.read()
+    fd.close()
+    resp      = MockResponse()
+    resp.text = content
+    return resp
+
+librato.connection.requests = MockRequest()
+
+class TestMetrics(unittest.TestCase):
   def setUp(self):
-    username    = "drio"
-    api_key     = "ABCDEFG"
-    hostname    = "localhost"
-    base_path   = "/v1/metrics"
-    self.libcon = librato.Connection(username, api_key, hostname, base_path)
+    self.api = librato.Connection("drio", "abcdef")
+
+  def tearDown(self):
+    pass
 
   def test_init(self):
-    assert type(self.libcon) is librato.Connection
+    assert type(self.api) == librato.Connection
 
+  def test_get_list_all_metrics(self):
+    results = self.api.list_metrics()
+    assert type(results) == list
+    assert len(results)  == 2
+    for m in results:
+      assert re.search('app_requests|server_temperature', m.name)
+
+if __name__ == '__main__':
+  unittest.main()
