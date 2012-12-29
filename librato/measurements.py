a7061be19e210b7c9c21cb80ee9d1b0ae515cdc9
David Rio Deiros <driodeiros@gmail.com>
2012-12-28 22:33:10 -0600

    No need to inherit JSON, we use .__dict__ instead.

--- librato/measurements.py
+++ librato/measurements.py
@@ -1,11 +1,6 @@
-from json import JSONEncoder
-
-class Measurement(JSONEncoder):
+class Measurement(object):
   def __init__(self, value, name=None, source=None, measure_time=None):
     self.value        = value
     self.name         = name
     self.source       = source
     self.measure_time = measure_time
-
-  def default(self, o):
-    return o.__dict__
