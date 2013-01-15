ee0d410078b0008de385b62de3135b7ba9e3e1e6
David Rio Deiros <driodeiros@gmail.com>
2013-01-15 16:37:38 -0600

    Adding LICENSE Preparing setup.py for publishing package.

--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
+#!/usr/bin/python
 
-setup(name="python-librato",
-      version="0.0.1",
-      description = "Python API Wrapper for Librato",
-      long_description="Python Wrapper for the Librato Metrics API: http://dev.librato.com/v1/metrics",
-      license="MIT??",
-      install_requires=['requests'],
-      author="XXXXXXXXXXXXX",
-      author_email="XXXXXXXXXXXXXXXXXX"
-      url = "http://dev.librato.com/v1/metrics",
-      packages = find_packages(),
-      keywords= "librato",
-      zip_safe = True)
+try:
+	from setuptools import setup, find_packages
+except ImportError:
+	from ez_setup import use_setuptools
+	use_setuptools()
+	from setuptools import setup, find_packages
+
+from librato import __version__
+
+setup(name = "librato-metrics",
+		version = __version__,
+		description = "Python API Wrapper for Librato",
+		long_description="Python Wrapper for the Librato Metrics API: http://dev.librato.com/v1/metrics",
+		author = "Joseph Ruscio",
+		author_email = "joe@librato.com",
+    maintainer = "David Rio-Deiros",
+    maintainer_email = "driodeiros@gmail.com",
+		url = "http://dev.librato.com/v1/metrics",
+		packages = ['librato'],
+		include_package_data = True,
+		license = 'LICENSE',
+		scripts = [],
+		platforms = 'Posix; MacOS X; Windows',
+		classifiers = [
+			'Development Status :: 3 - Alpha',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: MIT License',
+			'Operating System :: OS Independent',
+			'Topic :: Internet',
+		],
+		dependency_links = [],
+		install_requires = [],
+	)
