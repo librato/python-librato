23a727cff5920b64bda2f3337c90c2ea33f66609
David Rio Deiros <driodeiros@gmail.com>
2013-04-09 20:48:08 -0500

    0.4.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
   name = "librato-metrics",
-  version = "0.4",
+  version = "0.4.1",
   description = "Python API Wrapper for Librato",
   long_description="Python Wrapper for the Librato Metrics API: http://dev.librato.com/v1/metrics",
   author = "Librato",
