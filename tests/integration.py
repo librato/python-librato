3278f3cd22380e88b93ed0aa30e457f41eaa6e62
David Rio Deiros <driodeiros@gmail.com>
2016-12-02 10:53:51 -0500

    removing instrument integration tests

--- tests/integration.py
+++ tests/integration.py
@@ -197,35 +197,6 @@ class TestLibratoBasic(TestLibratoBase):
             self.conn.update(name, attributes=attrs)
         self.conn_sanitize.delete(name)
 
-    def test_adding_a_new_instrument_with_composite_metric_stream(self):
-        name = "my_INST_with_STREAMS"
-        ins = self.conn.create_instrument(name)
-        ins_id = ins.id
-        ins.new_stream(composite='s("cpu", "*")')
-        self.conn.update_instrument(ins)
-        ins = self.conn.get_instrument(ins.id)
-        assert ins.name == name
-        assert ins.id == ins_id
-        assert len(ins.streams) == 1
-        assert ins.streams[0].composite == 's("cpu", "*")'
-
-    def test_instrument_save_creates_new_record(self):
-        instrument_name = 'my instrument name'
-        i = librato.Instrument(self.conn, instrument_name)
-        assert i.id is None
-        i.save()
-        assert i.name == instrument_name
-
-    def test_instrument_save_updates_existing_record(self):
-        instrument_name = 'my instrument name'
-        i = self.conn.create_instrument(instrument_name)
-        assert i.name == instrument_name
-        i.name = 'NEW instrument name'
-        i.save()
-        i = self.conn.get_instrument(i.id)
-        assert i.name == 'NEW instrument name'
-
-
 class TestLibratoAlertsIntegration(TestLibratoBase):
 
     alerts_created_during_test = []
